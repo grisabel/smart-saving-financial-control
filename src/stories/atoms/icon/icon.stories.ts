594094fa313a3ca428f779d1a555be2bc5e6ae69
Delgado Trueba <jose.delgado@babel.es>
2024-03-08 19:23:26 +0100

    chore/icon: create component

---
+++ src/stories/atoms/icon/icon.stories.ts
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+
+import { IconComponent } from './icon.component';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+const meta: Meta<IconComponent> = {
+  title: 'Atoms/Icon',
+  component: IconComponent,
+  tags: ['autodocs'],
+  render: (args: IconComponent) => ({
+    props: {
+      backgroundColor: null,
+      ...args,
+    },
+  })
+};
+
+export default meta;
+type Story = StoryObj<IconComponent>;
+
+export const Primary: Story = {
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+};
