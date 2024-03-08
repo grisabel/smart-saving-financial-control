89f7e07ebdcc8cb6bc478c4ac5b7b723cf8d0544
Delgado Trueba <jose.delgado@babel.es>
2024-03-08 19:01:40 +0100

    chore: add button

--- src/stories/atoms/button/button.stories.ts
+++ src/stories/atoms/button/button.stories.ts
@@ -4,7 +4,7 @@ import { ButtonComponent } from './button.component';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta: Meta<ButtonComponent> = {
-  title: 'Example/Button',
+  title: 'Atoms/Button',
   component: ButtonComponent,
   tags: ['autodocs'],
   render: (args: ButtonComponent) => ({
@@ -12,18 +12,12 @@ const meta: Meta<ButtonComponent> = {
       backgroundColor: null,
       ...args,
     },
-  }),
-  argTypes: {
-    backgroundColor: {
-      control: 'color',
-    },
-  },
+  })
 };
 
 export default meta;
 type Story = StoryObj<ButtonComponent>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
     primary: true,
