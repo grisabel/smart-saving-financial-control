2d0943642b5cd1828e6852667ed8af8b4c1d764b
Delgado Trueba <jose.delgado@babel.es>
2024-03-09 18:09:20 +0100

    chore/modal: styles

--- src/stories/atoms/modal/modal.stories.ts
+++ src/stories/atoms/modal/modal.stories.ts
@@ -1,24 +1,31 @@
 import { type Meta, type StoryObj } from '@storybook/angular';
 
 import { ModalComponent } from './modal.component';
-import { Component, signal } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-modal-story',
   standalone: true,
   imports: [CommonModule, ModalComponent],
-  template: `<app-modal [open]="open" (onClose)="open.set(false)" />`,
+  template: `<app-modal [open]="open" (onClose)="onCloseHandler()" />`,
   styleUrls: [],
 })
 export class ModalComponentStory {
   open = signal(true);
+  @Output() onClose: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  onCloseHandler() {
+    this.open.set(false);
+    this.onClose.emit();
+  }
 }
 
 const meta: Meta<ModalComponentStory> = {
   title: 'Atoms/Modal',
   component: ModalComponentStory,
   tags: ['autodocs'],
+  argTypes: { onClose: { action: 'onClose' } },
 };
 
 export default meta;
