447fe4b0e618731d7b4b70e326233f504983e7b3
Delgado Trueba <jose.delgado@babel.es>
2024-03-09 11:41:03 +0100

    chore/dropdown: add signal

--- src/stories/atoms/inputs/dropdown/dropdown.component.ts
+++ src/stories/atoms/inputs/dropdown/dropdown.component.ts
@@ -1,10 +1,12 @@
 import {
+  ChangeDetectionStrategy,
   Component,
   ElementRef,
   EventEmitter,
   Input,
   Output,
   ViewChild,
+  signal,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -21,6 +23,7 @@ interface InputOption {
   imports: [CommonModule, IconComponent],
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DropdownComponent {
   @Input({ required: true }) id!: string;
@@ -32,15 +35,15 @@ export class DropdownComponent {
 
   @ViewChild('dropdownRef') dropdownRef!: ElementRef<HTMLDivElement>;
 
-  openDropdown: boolean = false;
-  inputText: string = '';
-  optionFocus: number = -1;
+  openDropdown = signal(false);
+  inputText = signal('');
+  optionFocus = signal(-1);
 
   constructor() {}
 
   ngOnInit() {
-    this.inputText = this._defaultOptionLabel(this.options, this.value);
-    this.optionFocus = this._defaultOptionFocus(this.options, this.value);
+    this.inputText.set(this._defaultOptionLabel(this.options, this.value));
+    this.optionFocus.set(this._defaultOptionFocus(this.options, this.value));
   }
 
   _defaultOptionFocus(
@@ -82,20 +85,20 @@ export class DropdownComponent {
   }
 
   _handleKeyUpDropdownItem() {
-    if (this.optionFocus <= 1) {
-      this.optionFocus = 0;
+    if (this.optionFocus() <= 1) {
+      this.optionFocus.set(0);
       return;
     }
-    this.optionFocus = this.optionFocus - 1;
+    this.optionFocus.update((value) => value - 1);
     return;
   }
 
   _handleKeyDownDropdownItem(optionsLength: number) {
-    if (this.optionFocus === optionsLength - 1) {
-      this.optionFocus = optionsLength - 1;
+    if (this.optionFocus() === optionsLength - 1) {
+      this.optionFocus.set(optionsLength - 1);
       return;
     }
-    this.optionFocus = this.optionFocus + 1;
+    this.optionFocus.update((value) => value + 1);
     return;
   }
 
@@ -107,15 +110,15 @@ export class DropdownComponent {
     } else if (event.code === 'ArrowUp') {
       this._handleKeyUpDropdownItem();
     } else if (event.code === 'Enter') {
-      if (this.options && this.optionFocus !== -1) {
-        this.onClickDropdownItem(this.options[this.optionFocus]);
+      if (this.options && this.optionFocus() !== -1) {
+        this.onClickDropdownItem(this.options[this.optionFocus()]);
         this.dropdownRef?.nativeElement?.blur();
       }
     }
   }
 
   onClickDropdownItem(option: InputOption) {
-    this.inputText = option.label;
+    this.inputText.set(option.label);
     this.dropdownRef?.nativeElement?.blur();
 
     this.onChange.emit({
@@ -126,8 +129,8 @@ export class DropdownComponent {
   onResetDropdown(event: MouseEvent) {
     event.stopPropagation();
 
-    this.inputText = '';
-    this.optionFocus = -1;
+    this.inputText.set('');
+    this.optionFocus.set(-1);
     this.dropdownRef?.nativeElement?.blur();
 
     this.onChange.emit({
