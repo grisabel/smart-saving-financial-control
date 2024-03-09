5d60f4c6144d79e373c7e37c77d87fd8f9b84d12
Delgado Trueba <jose.delgado@babel.es>
2024-03-09 07:56:41 +0100

    chore/button: create component

--- src/stories/atoms/button/icon.stories.ts
+++ src/stories/atoms/button/icon.stories.ts
@@ -20,27 +20,6 @@ type Story = StoryObj<ButtonComponent>;
 
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: 'Button',
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
+    name: 'calendar',
   },
 };
