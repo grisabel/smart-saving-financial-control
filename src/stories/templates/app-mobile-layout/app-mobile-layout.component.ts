759a2be4498796b8b5fce2fd078b03711f36a316
Delgado Trueba <jose.delgado@babel.es>
2024-03-10 21:48:07 +0100

    feat/CF-1.3: mobile layout

---
+++ src/stories/templates/app-mobile-layout/app-mobile-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-app-mobile-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app-mobile-layout.component.html',
+  styleUrls: ['./app-mobile-layout.component.scss']
+})
+export class AppMobileLayoutComponent {
+
+}
---
+++ src/stories/templates/app-mobile-layout/app-mobile-layout.stories.ts
@@ -0,0 +1,41 @@
+import {
+  componentWrapperDecorator,
+  type Meta,
+  type StoryObj,
+} from '@storybook/angular';
+
+import { AppMobileLayoutComponent } from './app-mobile-layout.component';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+const meta: Meta<AppMobileLayoutComponent> = {
+  title: 'Templates/AppMobileLayout',
+  component: AppMobileLayoutComponent,
+  tags: ['autodocs'],
+  render: (args: AppMobileLayoutComponent) => ({
+    props: {
+      backgroundColor: null,
+      ...args,
+    },
+  }),
+  decorators: [
+    componentWrapperDecorator(
+      (story: any) => `<div style="height: 100dvh">${story}</div>`
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<AppMobileLayoutComponent>;
+
+export const AppMobileLayoutExample: Story = {
+  render: (args: any) => ({
+    props: {
+      ...args,
+    },
+    template: `<app-app-mobile-layout>
+      <p slot="menu">menu</p>
+      <p slot="content">content</p>
+    </app-app-mobile-layout>`,
+  }),
+  args: {},
+};
