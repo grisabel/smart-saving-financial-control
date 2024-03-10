53547d30459aea98193c2f16f48985c548bdb3e9
Delgado Trueba <jose.delgado@babel.es>
2024-03-10 22:44:26 +0100

    feat/CF-1.3: create menu

---
+++ src/stories/molecules/menu/menu-mobile/menu-mobile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-menu-mobile',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './menu-mobile.component.html',
+  styleUrls: ['./menu-mobile.component.scss']
+})
+export class MenuMobileComponent {
+
+}
