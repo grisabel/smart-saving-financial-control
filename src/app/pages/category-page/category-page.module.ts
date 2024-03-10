d6e500481971496ec4f0e65e8bd406d12e051f48
Delgado Trueba <jose.delgado@babel.es>
2024-03-10 19:50:49 +0100

    feat/CF-4.1 category page

--- src/app/pages/category-page/category-page.module.ts
+++ src/app/pages/category-page/category-page.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { CategoryPageComponent } from './category-page.component';
 
-
+import { CategoryPageRoutingModule } from './category-page-routing.module';
 
 @NgModule({
-  declarations: [
-    CategoryPageComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [CategoryPageComponent],
+  imports: [CommonModule, CategoryPageRoutingModule],
 })
-export class CategoryPageModule { }
+export class CategoryPageModule {}
