af27cdaa3b731875fcbd262ca072836e95624a6a
Delgado Trueba <jose.delgado@babel.es>
2024-03-10 20:46:03 +0100

    feat/CF-4.1 back btn

--- src/app/pages/category-page/category-page.component.ts
+++ src/app/pages/category-page/category-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, WritableSignal, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { GLOBAL_APP_ROUTES } from '@app/app-routes';
 
 @Component({
   selector: 'app-category-page',
@@ -7,4 +9,10 @@ import { Component, WritableSignal, signal } from '@angular/core';
 })
 export class CategoryPageComponent {
   openAddCategory: WritableSignal<boolean> = signal(false);
+
+  constructor(private router: Router) {}
+
+  handleBack() {
+    this.router.navigate([GLOBAL_APP_ROUTES.globalPosition]);
+  }
 }
