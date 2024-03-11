f0006fbf3c97722e3ed10f7af438fa0e8e2a0897
Delgado Trueba <jose.delgado@babel.es>
2024-03-11 22:02:05 +0100

    feat/CF-1.3: create divisor

---
+++ src/stories/atoms/divisor/divisor.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-divisor',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './divisor.component.html',
+  styleUrls: ['./divisor.component.scss'],
+})
+export class DivisorComponent {}
