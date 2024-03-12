786696cd6129e9f896a90a735a4c9219fce066da
IsabelGR <grisabel97@outlook.com>
2024-03-12 23:10:29 +0100

    chore/categoryBtn: category btn component

---
+++ src/stories/atoms/buttons/category-btn/category-btn.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IconComponent } from '@stories/atoms/icon/icon.component';
+
+@Component({
+  selector: 'app-category-btn',
+  standalone: true,
+  imports: [CommonModule, IconComponent],
+  templateUrl: './category-btn.component.html',
+  styleUrls: ['./category-btn.component.scss'],
+})
+export class CategoryBtnComponent {
+  backgroundColor = '';
+
+  @Input() iconName: string = '';
+  @Output() onClick: EventEmitter<void> = new EventEmitter<void>();
+
+  ngOnInit() {
+    this.generateContrastingColor();
+  }
+
+  handleClick(): void {
+    this.onClick.emit();
+  }
+
+  generateContrastingColor() {
+    const r = Math.floor(Math.random() * 156);
+    const g = Math.floor(Math.random() * 156);
+    const b = Math.floor(Math.random() * 156);
+
+    const toHex = (c: number) => {
+      const hex = c.toString(16);
+      return hex.length == 1 ? '0' + hex : hex;
+    };
+
+    this.backgroundColor = '#' + toHex(r) + toHex(g) + toHex(b);
+  }
+}
