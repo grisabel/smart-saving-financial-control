2958a5cb83fc9ecaed9b2f585911e499530db911
IsabelGR <grisabel97@outlook.com>
2024-03-14 20:35:03 +0100

    chore/circleBtn

---
+++ src/stories/atoms/buttons/circle-btn/circle-btn.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IconComponent } from '@stories/atoms/icon/icon.component';
+
+@Component({
+  selector: 'app-circle-btn',
+  standalone: true,
+  imports: [CommonModule, IconComponent],
+  templateUrl: './circle-btn.component.html',
+  styleUrls: ['./circle-btn.component.scss'],
+})
+export class CircleBtnComponent {
+  @Input() iconName: string = '';
+  @Output() onClick: EventEmitter<void> = new EventEmitter<void>();
+
+  handleClick(): void {
+    this.onClick.emit();
+  }
+}
