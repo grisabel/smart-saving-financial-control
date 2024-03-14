c9b82e08827496402ef9baf1d12031e016bcfbe1
IsabelGR <grisabel97@outlook.com>
2024-03-14 20:35:03 +0100

    chore/circleBtn

---
+++ src/stories/atoms/buttons/circle-btn/circle-btn.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CircleBtnComponent } from './circle-btn.component';
+
+describe('CircleBtnComponent', () => {
+  let component: CircleBtnComponent;
+  let fixture: ComponentFixture<CircleBtnComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [CircleBtnComponent]
+    });
+    fixture = TestBed.createComponent(CircleBtnComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
