d3564bab2ff80c3fdc4249e71a31434ff27f6122
Delgado Trueba <jose.delgado@babel.es>
2024-03-15 17:41:52 +0100

    chore/calendar-range: first style

--- src/stories/atoms/inputs/calendar-range-picker/calendar-range-picker.component.ts
+++ src/stories/atoms/inputs/calendar-range-picker/calendar-range-picker.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardBaseComponent } from '@stories/atoms/card/card-base/card-base.component';
+import { IconComponent } from '@stories/atoms/icon/icon.component';
 
 @Component({
   selector: 'app-calendar-range-picker',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardBaseComponent, IconComponent],
   templateUrl: './calendar-range-picker.component.html',
-  styleUrls: ['./calendar-range-picker.component.scss']
+  styleUrls: ['./calendar-range-picker.component.scss'],
 })
-export class CalendarRangePickerComponent {
-
-}
+export class CalendarRangePickerComponent {}
