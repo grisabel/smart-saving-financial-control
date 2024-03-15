02c3150932751f372bb55a54583916bb725aafc4
Delgado Trueba <jose.delgado@babel.es>
2024-03-15 18:03:20 +0100

    chore/calendar-range: style II

--- src/stories/atoms/inputs/calendar-range-picker/calendar-range-picker.component.ts
+++ src/stories/atoms/inputs/calendar-range-picker/calendar-range-picker.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardBaseComponent } from '@stories/atoms/card/card-base/card-base.component';
 import { IconComponent } from '@stories/atoms/icon/icon.component';
+import { CircleBtnComponent } from '@stories/atoms/buttons/circle-btn/circle-btn.component';
 
 @Component({
   selector: 'app-calendar-range-picker',
   standalone: true,
-  imports: [CommonModule, CardBaseComponent, IconComponent],
+  imports: [CommonModule, CardBaseComponent, IconComponent, CircleBtnComponent],
   templateUrl: './calendar-range-picker.component.html',
   styleUrls: ['./calendar-range-picker.component.scss'],
 })
