b6784e7833febed4e062db6bdd70fc77d877c8c1
Delgado Trueba <jose.delgado@babel.es>
2024-03-16 12:20:23 +0100

    feat/CF-1.3: styles balance concept

--- src/app/pages/global-postion-page/view/components/account-card/balance-concept/balance-concept.component.ts
+++ src/app/pages/global-postion-page/view/components/account-card/balance-concept/balance-concept.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+export enum BalanceConceptEnum {
+  income = 'income',
+  expense = 'expense',
+}
+
+type EnumToType<T> = {
+  [key in keyof T]: T[key];
+}[keyof T];
 
 @Component({
   selector: 'app-balance-concept',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './balance-concept.component.html',
-  styleUrls: ['./balance-concept.component.scss']
+  styleUrls: ['./balance-concept.component.scss'],
 })
 export class BalanceConceptComponent {
+  @Input() concept!: EnumToType<BalanceConceptEnum>;
 
+  ngOnChanges(changes: any) {
+    console.log(changes);
+  }
 }
