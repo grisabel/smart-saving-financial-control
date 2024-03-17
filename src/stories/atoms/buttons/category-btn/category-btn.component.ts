bdb68924c106920b5c6d1f78a22c4697c1408766
IsabelGR <grisabel97@outlook.com>
2024-03-17 21:37:08 +0100

    feat/CF-1.3: change generate background

--- src/stories/atoms/buttons/category-btn/category-btn.component.ts
+++ src/stories/atoms/buttons/category-btn/category-btn.component.ts
@@ -27,15 +27,32 @@ export class CategoryBtnComponent {
   }
 
   generateContrastingColor() {
-    const r = Math.floor(Math.random() * 156);
-    const g = Math.floor(Math.random() * 156);
-    const b = Math.floor(Math.random() * 156);
+    const highValue = Math.floor(Math.random() * (256 - 129)) + 129;
 
-    const toHex = (c: number) => {
-      const hex = c.toString(16);
-      return hex.length == 1 ? '0' + hex : hex;
-    };
+    const midValue = Math.floor(Math.random() * 256);
+    const lowValue = Math.floor(Math.random() * 128);
 
+    let [r, g, b] = [midValue, midValue, midValue];
+    const highComponent = Math.floor(Math.random() * 3);
+    switch (highComponent) {
+      case 0:
+        r = highValue;
+        g = Math.random() > 0.5 ? midValue : lowValue;
+        b = g === midValue ? lowValue : midValue;
+        break;
+      case 1:
+        g = highValue;
+        r = Math.random() > 0.5 ? midValue : lowValue;
+        b = r === midValue ? lowValue : midValue;
+        break;
+      case 2:
+        b = highValue;
+        r = Math.random() > 0.5 ? midValue : lowValue;
+        g = r === midValue ? lowValue : midValue;
+        break;
+    }
+
+    const toHex = (c: number) => c.toString(16).padStart(2, '0');
     this.backgroundColor = '#' + toHex(r) + toHex(g) + toHex(b);
   }
 }
