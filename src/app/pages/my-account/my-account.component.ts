a4f82234a128522b349c34a5988786397f073631
IsabelGR <grisabel97@outlook.com>
2024-03-17 16:42:00 +0100

    feat/CF-1.3: add content my account

--- src/app/pages/my-account/my-account.component.ts
+++ src/app/pages/my-account/my-account.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { GLOBAL_APP_ROUTES } from '@app/app-routes';
+
+const SRC_USER =
+  'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png';
 
 @Component({
   selector: 'app-my-account',
   templateUrl: './my-account.component.html',
-  styleUrls: ['./my-account.component.scss']
+  styleUrls: ['./my-account.component.scss'],
 })
 export class MyAccountComponent {
+  href = '/global-position';
+  labelLink = 'Mi Cuenta';
 
+  src = SRC_USER;
+  //todo El nombre y objetivo vendrán de un servicio
+  userName = 'Pepe García Sánchez';
+  objective = 'Jubilación';
+  //todo traducciones
+  categories = 'Categorías';
+  mydata = 'Mis datos';
+  unsubscribe = 'Darse de baja';
+  logout = 'Cerrar Sesión';
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  handleClickCategories() {
+    this.router.navigate([GLOBAL_APP_ROUTES.categories]);
+  }
+
+  handleClickMyData() {
+    this.router.navigate([GLOBAL_APP_ROUTES.accountDetail]);
+  }
+
+  //todo llamada servicio unsubscribe y redirigir a login
+  handleClickUnsubscribe() {}
+
+  //todo llamada servicio logout y redirigir a login
+  handleClickLogout() {}
 }
--- src/app/pages/my-account/my-account.module.ts
+++ src/app/pages/my-account/my-account.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { MyAccountRoutingModule } from './my-account-routing.module';
 import { MyAccountComponent } from './my-account.component';
-
+import { BackLinkComponent } from '@stories/atoms/links/back-link/back-link.component';
+import { UserInfoComponent } from '@stories/atoms/user-info/user-info.component';
+import { ActionBtnComponent } from '@stories/atoms/buttons/action-btn/action-btn.component';
 
 @NgModule({
-  declarations: [
-    MyAccountComponent
-  ],
+  declarations: [MyAccountComponent],
   imports: [
     CommonModule,
-    MyAccountRoutingModule
-  ]
+    MyAccountRoutingModule,
+    BackLinkComponent,
+    UserInfoComponent,
+    ActionBtnComponent,
+  ],
 })
-export class MyAccountModule { }
+export class MyAccountModule {}
