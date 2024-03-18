1bbedc7631df38eaee789ba17070f30223c57d5c
IsabelGR <grisabel97@outlook.com>
2024-03-18 19:01:30 +0100

    chore/inputNumber: component

---
+++ src/stories/atoms/inputs/input-number/input-number.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputBaseComponent } from '../input-base/input-base.component';
+import { IconComponent } from '@stories/atoms/icon/icon.component';
+
+@Component({
+  selector: 'app-input-number',
+  standalone: true,
+  imports: [CommonModule, InputBaseComponent, IconComponent],
+  templateUrl: './input-number.component.html',
+  styleUrls: [],
+})
+export class InputNumberComponent {
+  @Input() id?: string;
+  @Input() label?: string;
+  @Input() placeholder?: string;
+  @Input() value?: number;
+  @Input() disabled?: boolean;
+  @Input() name?: string;
+  @Output() onChange: EventEmitter<number> = new EventEmitter<number>();
+}
