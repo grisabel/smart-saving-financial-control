e0a3c9138cb43ff49ad5c71a5cde09c00b809808
Delgado Trueba <jose.delgado@babel.es>
2024-03-20 21:12:47 +0100

    feat/CF-2.1: refactor folder

--- src/app/pages/add-concept-modal/add-concept-modal.module.ts
+++ src/app/pages/add-concept-modal/add-concept-modal.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { environment } from 'src/environments/environment';
+
 import { ModalStepsComponent } from '@stories/atoms/modals/modal-steps/modal-steps.component';
-import { AddConceptBaseModalComponent } from './components/add-concept-base-modal/view/add-concept-base-modal.component';
 
 import { DropdownComponent } from '@stories/atoms/inputs/dropdown/dropdown.component';
 import { SelectorCategoryBtnComponent } from '@stories/molecules/selector-category-btn/selector-category-btn.component';
@@ -10,14 +11,17 @@ import { InputNumberComponent } from '@stories/atoms/inputs/input-number/input-n
 import { InputTextDateComponent } from '@stories/atoms/inputs/input-text-date/input-text-date.component';
 import { InputTextAreaComponent } from '@stories/atoms/inputs/input-text-area/input-text-area.component';
 import { CategoryBtnComponent } from '@stories/atoms/buttons/category-btn/category-btn.component';
-import { FinancialControlInterfaceService } from './components/add-concept-base-modal/data/repository/financial-control/financial-control-interface.service';
-import { environment } from 'src/environments/environment';
-import { FinancialControlMockService } from './components/add-concept-base-modal/data/repository/financial-control/financial-control-mock.service';
-import { FinancialControlHttpService } from './components/add-concept-base-modal/data/repository/financial-control/financial-control-http.service';
-import { AccountStepComponent } from './components/add-concept-base-modal/view/components/account-step/account-step.component';
-import { CategoryStepComponent } from './components/add-concept-base-modal/view/components/category-step/category-step.component';
-import { ConceptStepComponent } from './components/add-concept-base-modal/view/components/concept-step/concept-step.component';
-import { SummaryStepComponent } from './components/add-concept-base-modal/view/components/summary-step/summary-step.component';
+
+import { FinancialControlInterfaceService } from './data/repository/financial-control/financial-control-interface.service';
+import { FinancialControlHttpService } from './data/repository/financial-control/financial-control-http.service';
+import { FinancialControlMockService } from './data/repository/financial-control/financial-control-mock.service';
+
+import { AddConceptBaseModalComponent } from './view/add-concept-base-modal.component';
+import { AccountStepComponent } from './view/components/account-step/account-step.component';
+import { CategoryStepComponent } from './view/components/category-step/category-step.component';
+import { ConceptStepComponent } from './view/components/concept-step/concept-step.component';
+import { SummaryStepComponent } from './view/components/summary-step/summary-step.component';
+
 @NgModule({
   declarations: [
     AddConceptBaseModalComponent,
