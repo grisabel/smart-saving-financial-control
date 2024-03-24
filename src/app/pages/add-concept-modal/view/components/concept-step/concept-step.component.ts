7360b5f235be32db6788862121292a303a1cae4d
Delgado Trueba <jose.delgado@babel.es>
2024-03-24 09:18:18 +0100

    chore/disableBtn: add validation

--- src/app/pages/add-concept-modal/view/components/concept-step/concept-step.component.ts
+++ src/app/pages/add-concept-modal/view/components/concept-step/concept-step.component.ts
@@ -1,5 +1,29 @@
 import { Component } from '@angular/core';
 import { AddConceptStoreService } from '../../store/add-concept-store.service';
+import { DateTimeModel } from '@app/utils/Datetime/DatetimeInterfaceService';
+import DateTimeService from '@app/utils/Datetime/DatetimeService';
+
+const getRuleDateUntilToday = (() => {
+  return {
+    validate: (_inputValue: string): boolean => {
+      const dateTime: DateTimeModel = {
+        date: _inputValue,
+        format: 'dd/MM/yyyy',
+      };
+      if (!DateTimeService.isValid(dateTime)) {
+        return true;
+      }
+
+      const currentDate = DateTimeService.currentDate();
+      const isValid = DateTimeService.validate(
+        dateTime,
+        currentDate,
+        DateTimeService.VALIDATE_SET.UNTIL
+      );
+      return isValid;
+    },
+  };
+})();
 
 @Component({
   selector: 'app-concept-step',
@@ -10,12 +34,14 @@ export class ConceptStepComponent {
   inputNumberLabel = 'Importe';
   inputDateLabel = 'Fecha';
   inputCommentLabel = 'Comentario';
+  isValidDate: boolean = true;
 
   constructor(private addConceptStoreService: AddConceptStoreService) {}
 
   valueDate = this.addConceptStoreService.date() ?? '';
   handleValueDate(value: string) {
     this.addConceptStoreService.date.set(value);
+    this.isValidDate = getRuleDateUntilToday.validate(value);
   }
 
   valueComment = this.addConceptStoreService.note() ?? '';
