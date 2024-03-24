87b55cdb2bd5ce6449ad78d0b5b45e62a96bc06d
Delgado Trueba <jose.delgado@babel.es>
2024-03-24 11:40:15 +0100

    feat/CF-3.1 add loginUseCase

--- src/app/domain/Session/session-use-case.service.ts
+++ src/app/domain/Session/session-use-case.service.ts
@@ -1,16 +1,12 @@
 import { Injectable } from '@angular/core';
 import { SessionInterfaceService } from '@app/repository/Session/session-interface.service';
+import { UserInterfaceService } from '@app/repository/User/user-interface.service';
 import { LoadingService } from '@app/services/Loading/loading.service';
+import { CommonStoreService } from '@app/store/Common/common-store.service';
 import { environment } from 'src/environments/environment';
 
-interface SummaryParams {
-  requestModel: null;
-  view: {
-    setLoading: (value: boolean) => void;
-  };
-}
-
 const LOCAL_STORAGE_KEYS = {
+  accessToken: 'accessToken',
   refreshToken: 'refreshToken',
 };
 
@@ -19,12 +15,26 @@ const LOCAL_STORAGE_KEYS = {
 })
 export class SessionUseCaseService {
   constructor(
+    private commonStore: CommonStoreService,
+    private userInterfaceService: UserInterfaceService,
     private sessionService: SessionInterfaceService,
     private loadingService: LoadingService
   ) {}
 
-  private getRefershToken() {
-    return window.localStorage.getItem(LOCAL_STORAGE_KEYS.refreshToken) ?? '';
+  loginUser() {
+    this.loadingService.show();
+
+    this.userInterfaceService
+      .info()
+      .then((userInfo) => {
+        this.commonStore.userInfo.set(userInfo);
+      })
+      .catch(() => {
+        this.logout();
+      })
+      .finally(() => {
+        this.loadingService.hide();
+      });
   }
 
   logout(): Promise<void> {
@@ -37,5 +47,13 @@ export class SessionUseCaseService {
           document.location.href = environment.data.logoutUrl;
         });
     });
+  }
+
+  private getRefershToken() {
+    return window.localStorage.getItem(LOCAL_STORAGE_KEYS.refreshToken) ?? '';
+  }
+
+  private getAccessToken() {
+    return window.localStorage.getItem(LOCAL_STORAGE_KEYS.accessToken) ?? '';
   }
 }
