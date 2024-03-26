b668be213a5116625876969d13f6b32d05ff8287
Delgado Trueba <jose.delgado@babel.es>
2024-03-26 22:37:00 +0100

    chore/react-integration: add pages

--- src/app/pages/account-detail/account-detail-routing.module.ts
+++ src/app/pages/account-detail/account-detail-routing.module.ts
@@ -2,7 +2,36 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountDetailComponent } from './account-detail.component';
 
-const routes: Routes = [{ path: '', component: AccountDetailComponent }];
+import {
+  WebComponentWrapper,
+  WebComponentWrapperOptions,
+} from '@angular-architects/module-federation-tools';
+
+const routes: Routes = [
+  { path: '', component: AccountDetailComponent },
+  {
+    path: '1',
+    component: WebComponentWrapper,
+    data: {
+      type: 'module',
+      remoteEntry: 'http://localhost:4202/assets/remoteReportsMfe.js',
+      exposedModule: './CategoriesReport',
+
+      elementName: 'categories-report-mfe',
+    } as WebComponentWrapperOptions,
+  },
+  {
+    path: '2',
+    component: WebComponentWrapper,
+    data: {
+      type: 'module',
+      remoteEntry: 'http://localhost:4202/assets/remoteReportsMfe.js',
+      exposedModule: './CategoriesDetailsReport',
+
+      elementName: 'categories-details-report-mfe',
+    } as WebComponentWrapperOptions,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
